5109fd65ee0f716fcfff4817a669fed458b1932d
Boris Serdiuk <serdiuk@amazon.com>
2022-08-22 22:54:01 +0200

    initial code

---
+++ scripts/compile-styles.js
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { buildThemedComponentsInternal } from "@cloudscape-design/theming-build";
+
+await buildThemedComponentsInternal({
+  componentsOutputDir: "lib/components",
+});
