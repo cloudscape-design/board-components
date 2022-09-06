279048b2699e10ae2d5d8c72b518083d2ca3f7a5
Florian Dreschner <fdresch@amazon.com>
2022-09-06 14:20:29 +0200

    test: Add Vitest as test runner (#6)
    * test: Add Vitest as test runner
    
    * test: Add dummy test

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
+});
