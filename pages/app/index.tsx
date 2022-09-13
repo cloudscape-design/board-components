4387ca6bb5d60b4063ea15ccae954ef87d5bd70d
Florian Dreschner <fdresch@amazon.com>
2022-09-13 10:43:22 +0200

    chore suppport folders (#12)
    * chore: Support folders in dev pages
    
    * feat: Parse any path into Page component
    
    * fix: Remove unnecessary comma in pattern

---
+++ pages/app/index.tsx
@@ -0,0 +1,45 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { HashRouter, Link, Route, Routes, useLocation } from "react-router-dom";
+import Page from "./page";
+import { pages } from "../pages";
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/*" element={<PageWithFallback />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+const Start = () => (
+  <>
+    <h1>Pages</h1>
+    <main>
+      <Index />
+    </main>
+  </>
+);
+
+const Index = () => (
+  <ul className="list">
+    {pages.map((page) => (
+      <li key={page}>
+        <Link to={`${page}`}>{page}</Link>
+      </li>
+    ))}
+  </ul>
+);
+
+const PageWithFallback = () => {
+  const { pathname: page } = useLocation();
+
+  if (!page || !page.includes(page)) {
+    return <span>Not Found</span>;
+  }
+
+  return <Page pageId={page} />;
+};
