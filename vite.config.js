dce724a4b87d5b085d3c5055b4f6aaec8cf52f3d
Boris Serdiuk <serdiuk@amazon.com>
2022-11-15 11:18:08 +0100

    chore: disable fast refresh feature (#40)

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ fastRefresh: false })],
   root: "./pages",
   base: "./",
   resolve: {
