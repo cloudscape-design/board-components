60a3e0ce5c3eae6c2a0d8f27f97f1efee13aa803
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-11-21 15:01:20 +0100

    Fix create-layout corner case

--- src/layout/calculations/create-layout.ts
+++ src/layout/calculations/create-layout.ts
@@ -13,29 +13,21 @@ function createGridItems(
   items: readonly DashboardLayoutProps.Item<unknown>[],
   columns: number
 ): readonly GridLayoutItem[] {
-  const result: GridLayoutItem[] = [];
+  const layoutItems: GridLayoutItem[] = [];
+  const colAffordance = [...Array(columns)].map(() => -1);
 
-  let currentRowIndex = 0;
-
-  for (let index = 0; index < items.length; index++) {
-    const { id, columnSpan, rowSpan, columnOffset } = items[index];
-    const next = index < items.length - 1 ? items[index + 1] : null;
-    const rightEdge = columnOffset + columnSpan - 1;
-    const remainingColumns = columns - rightEdge;
-
-    result.push({ id, width: columnSpan, height: rowSpan, x: columnOffset, y: currentRowIndex });
-
-    if (
-      remainingColumns <= 0 ||
-      (next && next.columnSpan > remainingColumns) ||
-      (next && next.columnOffset <= columnOffset)
-    ) {
-      // Increment row count if the next item will not fit in the same row anymore
-      currentRowIndex++;
+  for (const { id, columnSpan, rowSpan, columnOffset } of items) {
+    let itemRow = 0;
+    for (let col = columnOffset; col < columnOffset + columnSpan; col++) {
+      itemRow = Math.max(itemRow, colAffordance[col] + 1);
+    }
+    layoutItems.push({ id, width: columnSpan, height: rowSpan, x: columnOffset, y: itemRow });
+    for (let col = columnOffset; col < columnOffset + columnSpan; col++) {
+      colAffordance[col] = itemRow;
     }
   }
 
-  return result;
+  return layoutItems;
 }
 
 function createGridPlaceholders(rows: number, columns: number): readonly GridLayoutItem[] {
