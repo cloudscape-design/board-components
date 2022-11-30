c755796ab18b7102deaf51e0d99453bd791a2646
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-11-30 13:53:05 +0100

    Grid context

---
+++ src/internal/grid-context.ts
@@ -0,0 +1,17 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { createContext, useContext } from "react";
+
+export interface GridContext {
+  columns: number;
+  getWidth: (colspan: number) => number;
+  getHeight: (rowspan: number) => number;
+}
+
+const Context = createContext<GridContext | null>(null);
+
+export const GridContextProvider = Context.Provider;
+
+export function useGridContext() {
+  return useContext(Context);
+}
