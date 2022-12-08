a6e4c482492cdd877f76006195b132a34f17782e
Andrei Zhaleznichenka <andrei.zhaleznichenka@gmail.com>
2022-12-08 13:02:18 +0100

    Make active element fixed and add high z-index to overlay everything (#96)

--- src/item/index.tsx
+++ src/item/index.tsx
@@ -82,7 +82,8 @@ export default function DashboardItem({
   function getDragActiveStyles(transition: Transition): CSSProperties {
     return {
       transform: CSSUtil.Transform.toString(transition.transform),
-      position: transition?.sizeOverride ? "absolute" : undefined,
+      zIndex: 5000,
+      position: transition?.sizeOverride ? "fixed" : undefined,
       width: transition?.sizeOverride?.width,
       height: transition?.sizeOverride?.height,
     };
