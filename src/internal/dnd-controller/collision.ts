f3a1a1991f523fbbc0cb1b7ca20810df6a48f010
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-12-08 10:52:44 +0100

    Items can be inserted with the keyboard

--- src/internal/dnd-controller/collision.ts
+++ src/internal/dnd-controller/collision.ts
@@ -53,15 +53,29 @@ const getCollisions = (collisionRect: Rect, droppables: readonly [string, HTMLEl
 };
 
 export function getHoveredDroppables(
-  operation: "move" | "resize",
+  operation: "move" | "resize" | "insert",
   draggableElement: HTMLElement,
   coordinates: Coordinates,
   droppables: readonly [ItemId, HTMLElement][]
 ) {
   const activeRect = draggableElement.getBoundingClientRect();
-  const collisionRect =
-    operation === "resize"
-      ? { top: activeRect.top, left: activeRect.left, right: coordinates.x, bottom: coordinates.y }
-      : activeRect;
+
+  let collisionRect = { left: 0, top: 0, right: 0, bottom: 0 };
+
+  if (operation === "move") {
+    collisionRect = activeRect;
+  }
+  if (operation === "resize") {
+    collisionRect = { left: activeRect.left, top: activeRect.top, right: coordinates.x, bottom: coordinates.y };
+  }
+  if (operation === "insert") {
+    collisionRect = {
+      left: coordinates.x,
+      top: coordinates.y,
+      right: coordinates.x + activeRect.width,
+      bottom: coordinates.y + activeRect.height,
+    };
+  }
+
   return getCollisions(collisionRect, droppables);
 }
