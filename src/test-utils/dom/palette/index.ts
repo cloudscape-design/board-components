f4f158d0c4042ff58926590aa83cd5b91a766b8d
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-12-12 12:46:29 +0100

    Add palette-item to test-utils

--- src/test-utils/dom/palette/index.ts
+++ src/test-utils/dom/palette/index.ts
@@ -2,12 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 import { ComponentWrapper } from "@cloudscape-design/test-utils-core/dom";
 import paletteStyles from "../../../palette/styles.selectors.js";
-import DashboardItemWrapper from "../dashboard-item";
+import PaletteItemWrapper from "../palette-item";
 
 export default class PaletteWrapper extends ComponentWrapper {
   static rootSelector: string = paletteStyles.root;
 
-  findItemById(itemId: string): null | DashboardItemWrapper {
-    return this.findComponent(`[data-item-id=${itemId}]`, DashboardItemWrapper);
+  findItemById(itemId: string): null | PaletteItemWrapper {
+    return this.findComponent(`[data-item-id=${itemId}]`, PaletteItemWrapper);
   }
 }
