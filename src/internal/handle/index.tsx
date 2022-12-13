fa06d12ec4bda4e613bf29038cae6a8cbde9ec36
Boris Serdiuk <serdiuk@amazon.com>
2022-12-13 14:51:59 +0100

    fix: Only activate drag on left click

--- src/internal/handle/index.tsx
+++ src/internal/handle/index.tsx
@@ -1,11 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import clsx from "clsx";
-import { ButtonHTMLAttributes, ForwardedRef, forwardRef } from "react";
+import { ButtonHTMLAttributes, ForwardedRef, PointerEvent, forwardRef } from "react";
 import styles from "./styles.css.js";
 
 function Handle(props: ButtonHTMLAttributes<HTMLButtonElement>, ref: ForwardedRef<HTMLButtonElement>) {
-  return <button {...props} className={clsx(styles.handle, props.className)} ref={ref} />;
+  function handlePointerDown(event: PointerEvent<HTMLButtonElement>) {
+    if (event.button !== 0) {
+      return;
+    }
+    props.onPointerDown?.(event);
+  }
+
+  return (
+    <button {...props} onPointerDown={handlePointerDown} className={clsx(styles.handle, props.className)} ref={ref} />
+  );
 }
 
 export default forwardRef(Handle);
