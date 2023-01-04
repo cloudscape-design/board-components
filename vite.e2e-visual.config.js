dc80d73fa892b37f4814ba9cf68a36593e6730a2
Boris Serdiuk <serdiuk@amazon.com>
2023-01-04 15:47:14 +0000

    chore: Fix screenshots update job

--- vite.e2e-visual.config.js
+++ vite.e2e-visual.config.js
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import process from "node:process";
 import { defineConfig } from "vite";
 import functional from "./vite.e2e-functional.config.js";
 
@@ -8,6 +9,7 @@ export default defineConfig({
   ...functional,
   test: {
     ...functional.test,
+    update: process.env.UPDATE_SCREENSHOTS,
     include: ["./test/visual/**/*.test.ts"],
     setupFiles: [...functional.test.setupFiles, "./test/visual-test-setup.ts"],
   },
