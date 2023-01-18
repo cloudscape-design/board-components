fa09e12fbc3c9ee4cd62cff2a92984a1b5ac0a23
Andrei Zhaleznichenka <andrei.zhaleznichenka@gmail.com>
2023-01-18 13:24:56 +0100

    Replace reducer debounce with pointer throttle (#155)

---
+++ src/internal/utils/__tests__/throttle.test.ts
@@ -0,0 +1,59 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Mock, SpyInstance, afterAll, beforeEach, describe, expect, test, vi } from "vitest";
+import { throttle } from "../throttle";
+
+describe("throttle", () => {
+  let dateNowSpy: SpyInstance<[], number>;
+  let requestAnimationFrameSpy: SpyInstance<[FrameRequestCallback], number>;
+  let funcMock: Mock;
+  let tick: () => void;
+
+  beforeEach(() => {
+    let time = 0;
+    dateNowSpy = vi.spyOn(Date, "now").mockImplementation(() => time);
+    requestAnimationFrameSpy = vi.spyOn(window, "requestAnimationFrame").mockImplementation((callback) => {
+      tick = () => {
+        time++;
+        callback(time);
+      };
+      return time;
+    });
+    funcMock = vi.fn();
+  });
+
+  afterAll(() => {
+    dateNowSpy.mockRestore();
+    requestAnimationFrameSpy.mockRestore();
+  });
+
+  test("should run the client function synchronously for the first invocation", () => {
+    const throttled = throttle(funcMock, 50);
+
+    throttled("arg1", "arg2");
+
+    expect(funcMock).toBeCalledTimes(1);
+    expect(funcMock).toBeCalledWith("arg1", "arg2");
+    expect(dateNowSpy).toBeCalledTimes(1);
+    expect(requestAnimationFrameSpy).toBeCalledTimes(0);
+  });
+
+  test("should run the client function three times only", () => {
+    const throttled = throttle(funcMock, 25);
+
+    // Execution 1
+    throttled(`arg-${0}`);
+
+    // The function should execute every 25th iteration.
+    for (let i = 1; i <= 50; i++) {
+      throttled(`arg-${i}`);
+      tick();
+    }
+
+    expect(funcMock).toBeCalledTimes(3);
+    expect(funcMock).toBeCalledWith("arg-0");
+    expect(funcMock).toBeCalledWith("arg-25");
+    expect(funcMock).toBeCalledWith("arg-50");
+  });
+});
