75e5da4724317943969a994e8eef9828c9448169
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-01-19 10:49:41 +0100

    Add code comments for grid

--- src/internal/grid/grid.tsx
+++ src/internal/grid/grid.tsx
@@ -6,19 +6,22 @@ import { CSS as CSSUtil } from "@dnd-kit/utilities";
 import clsx from "clsx";
 import { Children } from "react";
 import { GridContextProvider } from "../grid-context";
+import { zipTwoArrays } from "../utils/zip-arrays";
 
 import { GridProps } from "./interfaces";
 import GridItem from "./item";
 import styles from "./styles.css.js";
-import { zipTwoArrays } from "./utils";
 
+/* Matches grid gap in CSS. */
 const GRID_GAP = 16;
+/* Matches grid-auto-rows in CSS. */
 const ROWSPAN_HEIGHT = 100;
 
 export default function Grid({ layout, children, columns, rows, transforms, inTransition }: GridProps) {
   const [gridWidth, containerQueryRef] = useContainerQuery((entry) => entry.contentBoxWidth, []);
   const zipped = zipTwoArrays(layout, Children.toArray(children));
 
+  // The below getters translate relative grid units into size/offset values in pixels.
   const getWidth = (colspan: number) => {
     colspan = Math.min(columns, colspan);
     const cellWidth = ((gridWidth || 0) - (columns - 1) * GRID_GAP) / columns;
@@ -33,27 +36,28 @@ export default function Grid({ layout, children, columns, rows, transforms, inTr
       <div ref={containerQueryRef} data-columns={columns} data-rows={rows} className={styles.grid}>
         {zipped.map(([item, children]) => {
           const contentTransform = transforms?.[item.id];
-
           const contentClassNames: string[] = [];
-          let contentStyle: React.CSSProperties = {};
+          const contentStyle: React.CSSProperties = {};
 
+          // When there is an active transition grid items use animations.
           if (inTransition) {
             contentClassNames.push(styles["in-transition"]);
           }
-          if (contentTransform && contentTransform.type === "move") {
+          // The moved items positions are altered with CSS transform.
+          if (contentTransform?.type === "move") {
             contentClassNames.push(styles.transformed);
-            contentStyle = {
-              transform: CSSUtil.Transform.toString({
-                x: getColOffset(contentTransform.x),
-                y: getRowOffset(contentTransform.y),
-                scaleX: 1,
-                scaleY: 1,
-              }),
-              width: getWidth(contentTransform.width) + "px",
-              height: getHeight(contentTransform.height) + "px",
-            };
+            contentStyle.transform = CSSUtil.Transform.toString({
+              x: getColOffset(contentTransform.x),
+              y: getRowOffset(contentTransform.y),
+              scaleX: 1,
+              scaleY: 1,
+            });
+            contentStyle.width = getWidth(contentTransform.width) + "px";
+            contentStyle.height = getHeight(contentTransform.height) + "px";
           }
-          if (contentTransform && contentTransform.type === "remove") {
+          // The item is removed from the DOM after animations play.
+          // During the animations the removed item is hidden with styles.
+          if (contentTransform?.type === "remove") {
             contentClassNames.push(styles.removed);
           }
 
