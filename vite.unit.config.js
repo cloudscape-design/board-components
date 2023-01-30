ff166145a66ead15d1b680de6c836e822a0f3a9c
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-01-30 17:10:28 +0100

    Import process

--- vite.unit.config.js
+++ vite.unit.config.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import process from "node:process";
 import { defineConfig } from "vite";
 import base from "./vite.config.js";
 
