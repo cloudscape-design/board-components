514db395ccc556b557f7ac74b4b448401aec8d0c
Boris Serdiuk <serdiuk@amazon.com>
2023-02-15 15:42:52 +0100

    resize window before test starts

--- test/setup-test.ts
+++ test/setup-test.ts
@@ -4,17 +4,17 @@
 import { BasePageObject } from "@cloudscape-design/browser-test-tools/page-objects";
 import useBrowser from "@cloudscape-design/browser-test-tools/use-browser";
 
+// Default window size to ensure 4-columns layout is used.
+const windowSize = { width: 1600, height: 800 };
+
 export function setupTest<P extends BasePageObject & { init?(): Promise<void> }>(
   url: string,
   PageClass: new (browser: WebdriverIO.Browser) => P,
   test: (page: P) => Promise<void>
 ) {
-  return useBrowser(async (browser) => {
+  return useBrowser(windowSize, async (browser) => {
     await browser.url(url);
     const page = new PageClass(browser);
-
-    // Default window size to ensure 4-columns layout is used.
-    await page.setWindowSize({ width: 1600, height: 800 });
     await page.waitForVisible("main");
 
     // Custom initialization.
