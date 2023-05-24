d234b7c61f0324e4f4ae49d5eb36ee94f20b4b28
Francesco Longo <flongo@amazon.de>
2023-05-24 15:31:51 +0200

    chore: Fake commit to trigger release

--- pages/screenshot-area.tsx
+++ pages/screenshot-area.tsx
@@ -1,6 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-
 import { ReactNode } from "react";
 
 export function ScreenshotArea({ children }: { children: ReactNode }) {
