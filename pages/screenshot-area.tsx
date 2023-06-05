c2e27baea26a3916f51a16afdd60847ded5c5b02
Aleksandra Danilina <29692746+Al-Dani@users.noreply.github.com>
2023-06-05 19:10:04 +0200

    chore: Fake commit to trigger release (#241)

--- pages/screenshot-area.tsx
+++ pages/screenshot-area.tsx
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
 import { ReactNode } from "react";
 
 export function ScreenshotArea({ children }: { children: ReactNode }) {
