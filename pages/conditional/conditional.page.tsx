6bd1891e06fa4a1a8c59d2ab4fbcd9f9e921c04c
Boris Serdiuk <serdiuk@amazon.com>
2023-06-16 11:25:50 +0200

    add use-case with conditional edit mode (#245)

---
+++ pages/conditional/conditional.page.tsx
@@ -0,0 +1,65 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { Box, Button, Container, Header } from "@cloudscape-design/components";
+import { useState } from "react";
+import { Board, BoardItem, BoardProps } from "../../lib/components";
+import { boardI18nStrings, boardItemI18nStrings } from "../shared/i18n";
+import { ItemData } from "../shared/interfaces";
+
+export default function Conditional() {
+  const [isEditable, setEditable] = useState(false);
+  const [items, setItems] = useState<ReadonlyArray<BoardProps.Item<ItemData>>>([
+    {
+      id: "one",
+      columnSpan: 2,
+      rowSpan: 4,
+      definition: {},
+      data: { title: "First container", description: "", content: "Content" },
+    },
+    {
+      id: "two",
+      columnSpan: 2,
+      definition: {},
+      data: { title: "Second container", description: "", content: "Content" },
+    },
+    {
+      id: "three",
+      columnSpan: 2,
+      definition: {},
+      data: { title: "Last container", description: "", content: "Content" },
+    },
+  ]);
+  return (
+    <main>
+      <Box margin="l">
+        <Header
+          variant="h1"
+          actions={
+            <Button data-testid="edit-toggle" onClick={() => setEditable((isEditable) => !isEditable)}>
+              {isEditable ? "Save" : "Edit"}
+            </Button>
+          }
+        >
+          Dashboard
+        </Header>
+        <Board
+          items={items}
+          renderItem={(item) =>
+            isEditable ? (
+              <BoardItem header={<Header>{item.data.title}</Header>} i18nStrings={boardItemI18nStrings}>
+                {item.data.content}
+              </BoardItem>
+            ) : (
+              <Container header={<Header>{item.data.title}</Header>} fitHeight={true}>
+                {item.data.content}
+              </Container>
+            )
+          }
+          i18nStrings={boardI18nStrings}
+          onItemsChange={(event) => setItems(event.detail.items)}
+          empty="empty"
+        />
+      </Box>
+    </main>
+  );
+}
---
+++ test/functional/board-layout/conditional-editing.test.ts
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { describe, expect, test } from "vitest";
+import createWrapper from "../../../lib/components/test-utils/selectors";
+import { setupTest } from "../../utils";
+import { DndPageObject } from "./dnd-page-object";
+
+const wrapper = createWrapper();
+const boardWrapper = createWrapper().findBoard();
+const boardItemSelector = wrapper.findBoardItem().toSelector();
+
+describe("conditional editing feature", () => {
+  test(
+    "item sizes is preserved between editable and static mode",
+    setupTest("/index.html#/conditional/conditional", DndPageObject, async (page) => {
+      const getWidgetSizes = async () => [
+        await page.getBoundingBox(boardWrapper.findItemById("one").toSelector()),
+        await page.getBoundingBox(boardWrapper.findItemById("two").toSelector()),
+        await page.getBoundingBox(boardWrapper.findItemById("three").toSelector()),
+      ];
+      const sizeBefore = await getWidgetSizes();
+      // no board items by default
+      await expect(page.getElementsCount(boardItemSelector)).resolves.toEqual(0);
+      await page.click('[data-testid="edit-toggle"]');
+      // board items are rendering after toggle the mode
+      await expect(page.getElementsCount(boardItemSelector)).resolves.toEqual(3);
+      await expect(getWidgetSizes()).resolves.toEqual(sizeBefore);
+      await page.click('[data-testid="edit-toggle"]');
+      // board items are hidden after toggling it back
+      await expect(page.getElementsCount(boardItemSelector)).resolves.toEqual(0);
+      await expect(getWidgetSizes()).resolves.toEqual(sizeBefore);
+    })
+  );
+});
