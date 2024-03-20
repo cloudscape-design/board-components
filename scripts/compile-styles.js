fd52d4021b59050d09361733e46a4004f6d2259a
Boris Serdiuk <serdiuk@amazon.com>
2024-03-20 11:32:14 +0100

    chore: Use simplified styles build (#296)

--- scripts/compile-styles.js
+++ scripts/compile-styles.js
@@ -1,12 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ThemeBuilder, buildThemedComponentsInternal } from "@cloudscape-design/theming-build";
+import { buildStyles } from "@cloudscape-design/theming-build/internal";
 
-await buildThemedComponentsInternal({
-  primary: new ThemeBuilder("unused", ":root", []).build(),
-  componentsOutputDir: "lib/components",
-  skip: ["design-tokens", "preset"],
-  scssDir: "src",
-  variablesMap: {},
-  exposed: [],
-});
+await buildStyles("src", "lib/components");
