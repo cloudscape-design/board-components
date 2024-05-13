8e563a7e681e124690173ef25c03cd3a74a3499f
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-05-13 13:50:47 +0200

    wip: parse rtl query param

--- pages/app/page.tsx
+++ pages/app/page.tsx
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
 import { Suspense } from "react";
+import { useSearchParams } from "react-router-dom";
 import { pagesMap } from "../pages";
 
 export interface PageProps {
@@ -8,6 +10,10 @@ export interface PageProps {
 }
 
 export default function Page({ pageId }: PageProps) {
+  const [searchParams] = useSearchParams();
+  const direction = searchParams.get("direction") ?? "ltr";
+  document.documentElement.setAttribute("dir", direction);
+
   const Component = pagesMap[pageId];
 
   return (
