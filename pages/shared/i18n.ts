d8981c9cad0300390ee4b29bf23b95a715c2dc02
Johannes Weber <jowejowe@amazon.com>
2025-02-27 10:14:22 +0100

    chore: Update board item's i18n strings
    Add instructions to temp disable keyboard navigation while resizing/moving.
    Why? A screen reader's keyboard navigation mode may intercept arrow key presses. We can't disable that with an appropriate role, that's why we extend the drag handlers description with that information.

--- pages/shared/i18n.ts
+++ pages/shared/i18n.ts
@@ -57,10 +57,10 @@ export const clientI18nStrings = {
 export const boardItemI18nStrings: BoardItemProps.I18nStrings = {
   dragHandleAriaLabel: "Drag handle",
   dragHandleAriaDescription:
-    "Use Space or Enter to activate drag, arrow keys to move, Space or Enter to submit, or Escape to discard.",
+    "Use Space or Enter to activate drag, arrow keys to move, Space or Enter to submit, or Escape to discard. Be sure to temporarily disable any screen reader navigation feature that may interfere with the functionality of the arrow keys.",
   resizeHandleAriaLabel: "Resize handle",
   resizeHandleAriaDescription:
-    "Use Space or Enter to activate resize, arrow keys to move, Space or Enter to submit, or Escape to discard.",
+    "Use Space or Enter to activate resize, arrow keys to move, Space or Enter to submit, or Escape to discard. Be sure to temporarily disable any screen reader navigation feature that may interfere with the functionality of the arrow keys.",
 };
 
 export const itemsPaletteI18nStrings: ItemsPaletteProps.I18nStrings<ItemData> = {
