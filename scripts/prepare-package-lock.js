9a0e3d7f944e1ee45a091ebb36bcac212516fead
Michael Dowse <dowsem@amazon.com>
2025-04-16 13:50:31 +0200

    chore: Upgrade package-lock to version 3
    - Sync package lock script with components repo

--- scripts/prepare-package-lock.js
+++ scripts/prepare-package-lock.js
@@ -3,38 +3,32 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import fs from "node:fs";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
+
+const lockfiles = ["package-lock.json"];
+
+for (const filename of lockfiles) {
+  unlock(filename);
+}
 
 /**
  * Remove specific @cloudscape-design/* packages where we should always use the latest minor release.
- * Also checks for any dependencies that incorrectly use CodeArtifact instead of npm.
  */
-const filename = require.resolve("../package-lock.json");
-const packageLock = require(filename);
+function unlock(filename) {
+  const packageLock = JSON.parse(fs.readFileSync(filename));
+  if (packageLock.lockfileVersion !== 3) {
+    throw Error("package-lock.json file is not version 3. Use regular npm to update the packages.");
+  }
 
-if (packageLock.lockfileVersion !== 2) {
-  throw new Error('package-lock.json must have "lockfileVersion": 2');
-}
-
-function unlock(packages) {
-  Object.keys(packages).forEach((dependencyName) => {
-    const dependency = packages[dependencyName];
-
-    if (dependencyName.includes("@cloudscape-design/")) {
-      delete packages[dependencyName];
-    } else if (dependency.resolved && dependency.resolved.includes("codeartifact.us-west-2.amazonaws.com")) {
-      throw Error(
-        "package-lock.json file contains a reference to CodeArtifact. Use regular npm to update the packages.",
-      );
-    }
+  Object.keys(packageLock.packages).forEach((dependencyName) => {
+    removeDependencies(dependencyName, packageLock.packages);
   });
 
-  return packages;
+  fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + "\n");
+  console.log(`Removed @cloudscape-design/ dependencies from ${filename} file`);
 }
 
-packageLock.packages = unlock(packageLock.packages);
-packageLock.dependencies = unlock(packageLock.dependencies);
-
-fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + "\n");
-console.log("Removed @cloudscape-design/ dependencies from package-lock file");
+function removeDependencies(dependencyName, packages) {
+  if (dependencyName.includes("@cloudscape-design/")) {
+    delete packages[dependencyName];
+  }
+}
