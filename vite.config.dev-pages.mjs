fefcc974f74e65fb3c7e2ebcd096dc62b53b1396
Joan Perals <jotresse@amazon.com>
2025-12-23 11:28:04 +0100

    chore: Add dev pages bundle to build artifacts (#385)

---
+++ vite.config.dev-pages.mjs
@@ -0,0 +1,25 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({ fastRefresh: false })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "pages/main.tsx"),
+      name: "BoardComponentsDevPages",
+      fileName: "main",
+    },
+    outDir: "lib/dev-pages/bundle",
+    rollupOptions: {
+      external: [/(?:\.\.\/)+?lib\/components/, /^@cloudscape-design\//, "react"],
+    },
+  },
+});
